69f3420f8348cb29499bc3129648ed2d9923a81d
Anton Babko <anton.babko@ukma.edu.ua>
2023-02-07 23:57:44 +0200

    small button view, my location button, map type button

---
+++ O-Weather/View/SmallButton.swift
@@ -0,0 +1,26 @@
+//
+//  SmallButton.swift
+//  O-Weather
+//
+//  Created by admin on 07.02.2023.
+//
+
+import UIKit
+
+@IBDesignable
+class SmallButton: UIButton {
+
+    override func prepareForInterfaceBuilder() {
+        configure()
+    }
+    
+    override func awakeFromNib() {
+        configure()
+    }
+    
+    func configure(){
+        clipsToBounds = true
+        layer.cornerRadius = frame.width/2
+        backgroundColor = .darkGray
+    }
+}
